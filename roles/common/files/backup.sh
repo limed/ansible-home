856296abed3b0d2fcf3fbf1272fd5d2109ecb9b1
limed <limed@sudoers.org>
2016-08-03 15:04:40 -0700

    Backup files to s3 bucket

---
+++ roles/common/files/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simple backup script to backup files to s3
+# Requires aws package to be installed
+
+set -eu
+
+backup_path="/srv/blackbox/Volume_1/backups/"
+s3_remote_bucket="limed-sudoers-backupbucket"
+
+# first things first delete all the cruft
+find ${backup_path} -name '*.DS_Store' -type f -delete
+aws s3 sync ${backup_path} s3://limed-sudoers-backupbucket --delete --quiet
